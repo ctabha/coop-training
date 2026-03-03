10822a9f7a5752f57a99cb665d5d9ae85eafbaec
ctabha <abhamt@gmail.com>
2026-03-03 17:58:37 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-set -e
+set -o errexit
 
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Install LibreOffice for DOCX->PDF conversion
 apt-get update
 apt-get install -y libreoffice
-
-pip install -r requirements.txt
